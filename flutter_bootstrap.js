c15d23a40e6624fb5d2a0e42cd06ade25b33a42d
fhmhud <fhm.hud@gmail.com>
2025-03-18 20:43:19 +0700

    0.9.10

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1398928399"
+    serviceWorkerVersion: "2224301895"
   }
 });
